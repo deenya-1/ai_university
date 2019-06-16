e94889aee9f60964c8d859f679d24540922075e7
deenya-1 <deenya@gmail.com>
2019-06-16 20:08:30 +0300

    task 12: fix bugs

--- test/task_12.py
+++ test/task_12.py
@@ -1,11 +1,9 @@
-list = [-1,20,30]
-
 def binary_search(list, elem):
     min_ind = 0
     max_ind = len(list) - 1
 
-    while (min_ind < max_ind):
-        i = round((max_ind-min_ind) / 2)
+    while min_ind < max_ind:
+        i = round(min_ind + (max_ind-min_ind) / 2)
         if list[i] < elem:
             min_ind = i+1
         elif list[i] > elem:
@@ -18,5 +16,12 @@ def binary_search(list, elem):
 
     return -1
 
+# Test
+list = []
+print(binary_search(list, 5))
 
-print(binary_search(list, 31))
+list = [5]
+print(binary_search(list, 5))
+
+list = [1,2,3,4,5,6,7,8,9]
+print(binary_search(list, 5))
